78a0ea5e0069ce97ba471f25c5ff6ab7dc07bc75
Eugene Obrezkov <ghaiklor@gmail.com>
2015-06-18 12:22:05 +0300

    Add tests for index file

--- test/index.js
+++ test/index.js
@@ -1,10 +1,21 @@
 var assert = require('chai').assert;
 var HashService = require('../');
+var BCryptHash = require('../lib/BCryptHash');
 
 describe('HashService', function () {
   it('Should properly export', function () {
     assert.isObject(HashService);
     assert.isFunction(HashService.create);
     assert.isFunction(HashService.BCrypt);
+  });
+
+  it('Should properly create bcrypt instance', function () {
+    assert.instanceOf(HashService.create('bcrypt'), BCryptHash);
+  });
+
+  it('Should properly throw Error if type is unrecognized', function () {
+    assert.throw(function () {
+      HashService.create('NOT_EXISTS');
+    }, Error);
   });
 });
