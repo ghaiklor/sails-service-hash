dd2e11f70d0eae1b0f973b30bac024f3f0b019c3
Eugene Obrezkov <ghaiklor@gmail.com>
2015-09-25 12:21:15 +0300

    Rewrite BaseHash to ES6

--- src/BaseHash.js
+++ src/BaseHash.js
@@ -1,41 +1,29 @@
-var _ = require('lodash');
+import _ from 'lodash';
 
-/**
- * BaseHash class
- * @param {Object} _config Additional configuration for hash
- * @constructor
- */
-function BaseHash(_config) {
-  this._config = {};
+export default class BaseHash {
+  constructor(config) {
+    this._config = {};
 
-  _.forOwn(_config, function (value, key) {
-    this.set(key, value);
-  }.bind(this));
+    _.assign(this._config, config);
+  }
+
+  /**
+   * Get configuration value
+   * @param {String} [path]
+   * @returns {*}
+   */
+  get(path) {
+    return typeof path === 'undefined' ? this._config : _.get(this._config, path);
+  }
+
+  /**
+   * Set new configuration value
+   * @param {String} path
+   * @param {*} value
+   * @returns {BaseHash}
+   */
+  set(path, value) {
+    _.set(this._config, path, value);
+    return this;
+  }
 }
-
-/**
- * Get configuration value
- * @param {String} [path]
- * @returns {*}
- */
-BaseHash.prototype.get = function (path) {
-  return typeof path === 'undefined' ? this._config : _.get(this._config, path);
-};
-
-/**
- * Set new configuration value
- * @param {String} path
- * @param {*} value
- * @returns {BaseHash}
- */
-BaseHash.prototype.set = function (path, value) {
-  _.set(this._config, path, value);
-  return this;
-};
-
-BaseHash.prototype.hash = _;
-BaseHash.prototype.hashSync = _;
-BaseHash.prototype.compare = _;
-BaseHash.prototype.compareSync = _;
-
-module.exports = BaseHash;
